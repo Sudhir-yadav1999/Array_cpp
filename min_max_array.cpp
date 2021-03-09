2cf14ba799263d3ba0563f844d5b89f6c08a98aa
Sudhir yadav <54906044+Sudhir-yadav1999@users.noreply.github.com>
2021-03-09 12:20:06 +0530

    Create min_max_array.cpp

---
+++ min_max_array.cpp
@@ -0,0 +1,54 @@
+-----------------------
+Min-Max using Brute Force
+-----------------------
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int min(int arr[],int n)
+{
+	int minn=arr[0];
+	for(int i=0;i<n;i++)
+	{
+	  if(arr[i]<minn)
+	  {
+	  	minn=arr[i];
+	  }
+	}
+	
+	return minn;
+}
+
+int max(int arr[],int n)
+{
+	int maxx=0;
+	for(int i=0;i<n;i++)
+	{
+	  if(arr[i]>maxx)
+	  {
+	  	maxx=arr[i];
+	  }
+	}
+	
+	
+	return maxx;
+}
+
+int main()
+{
+  int arr[10],n;
+  cout<<"Enter the size of the array :";
+  cin>>n;
+  for(int i=0;i<n;i++)
+  {
+  	cin>>arr[i];
+  }
+  
+  int mm=min(arr,n);
+  int mx=max(arr,n);
+
+  cout<<mm<<endl;
+  cout<<mx<<endl;
+  
+	return 0;
+}
