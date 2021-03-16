35a821ba4eb1734aea7d71c5c19c06c655871f6b
Sudhir yadav <54906044+Sudhir-yadav1999@users.noreply.github.com>
2021-03-16 14:41:55 +0530

    Update and rename Rotate_array_one.cpp to Rotate_array_right_multi.cpp

--- Rotate_array_right_multi.cpp
+++ Rotate_array_right_multi.cpp
@@ -47,3 +47,49 @@ int main()
 	return 0;
 	
 }
+
+
+-------------------------------------
+User input no of times array rotation
+-------------------------------------
+#include<iostream>
+using namespace std;
+
+void arr_rot(int arr1[],int size1, int rot)
+{
+	    int i;
+	    
+		for(int j=0;j<rot;j++)
+		{
+			int temp=arr1[size1];
+			
+			for(i=size1-1;i>=0;i--)
+	        {
+		    arr1[i+1]= arr1[i];
+		     
+	        } 
+	        arr1[++i]=temp;
+	        
+		}
+		for(int i=0;i<=size1;i++)
+		{
+			cout<<arr1[i]<<" ";
+		}
+	
+}
+
+int main()
+{
+	int arr1[]={1, 3, 4, 5, 7};
+	
+
+	int size1=sizeof(arr1)/sizeof(arr1[0]);
+	int rot;
+	cout<<"Enter the no of rotation you want to perform ";
+    	cin>>rot;
+	arr_rot(arr1,size1-1,rot);
+	
+	
+	return 0;
+	
+}
